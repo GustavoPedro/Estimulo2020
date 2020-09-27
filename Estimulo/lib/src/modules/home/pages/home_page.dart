4dcb856c23a052e53f0a9bd72fc2b441cb30ec6b
GustavoPedro <gustavopedrodesouza@hotmail.com>
2020-09-27 11:51:36 -0300

    Modelando menu

--- Estimulo/lib/src/modules/home/pages/home_page.dart
+++ Estimulo/lib/src/modules/home/pages/home_page.dart
@@ -37,19 +37,6 @@ class _HomePage extends State<HomePage> {
                   _scaffoldKey.currentState.openDrawer();
                 }),
             title: Text("Bem Vindo"),
-            actions: <Widget>[
-              IconButton(
-                  icon: Icon(Icons.exit_to_app),
-                  onPressed: () {
-                    _loginController.logout();
-                    Navigator.pushReplacement(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => AuthenticationPage(),
-                      ),
-                    );
-                  }),
-            ],
           ),
           body: Padding(
             padding: EdgeInsets.all(10),
@@ -83,49 +70,85 @@ class _HomePage extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     final _drawer = Drawer(
-      child: ListView(
-        padding: EdgeInsets.zero,
-        children: <Widget>[
-          DrawerHeader(
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Center(
-                  child: Image.asset(
-                    'lib/assets/images/logo.jpg',
-                    height: 90,
+      child: Column(
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(
+              width: MediaQuery.of(context).size.width * 0.85,
+              child: DrawerHeader(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("lib/assets/images/estimulo2020.jpeg"),
+                    alignment: Alignment.bottomCenter,
                   ),
+                ),
+                child: Text(""),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 2,
+            child: ListView(
+              children: [
+                ListTile(
+                  leading: Icon(Icons.group),
+                  title: Text("Mentoria Especifica"),
+                  selected: 1 == _selectedIndex,
+                  onTap: () {
+                    _onSelectItem(1);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.play_arrow),
+                  //title: Text(AppLocalizations.of(context).translate('home')),
+                  title: Text("Capacitação Geral"),
+                  selected: 0 == _selectedIndex,
+                  onTap: () {
+                    _onSelectItem(0);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.assignment),
+                  title: Text("Acompanhamento do Negócio"),
+                  selected: 2 == _selectedIndex,
+                  onTap: () {
+                    _onSelectItem(2);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.info),
+                  title: Text("Sobre Nós"),
+                  selected: 1 == _selectedIndex,
+                  onTap: () {
+                    _onSelectItem(1);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.person),
+                  //title: Text(AppLocalizations.of(context).translate('home')),
+                  title: Text("Perfil"),
+                  selected: 0 == _selectedIndex,
+                  onTap: () {
+                    _onSelectItem(0);
+                  },
+                ),
+                ListTile(
+                  leading: Icon(Icons.exit_to_app),
+                  title: Text("Sair"),
+                  selected: 2 == _selectedIndex,
+                  onTap: () {
+                    _loginController.logout();
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => AuthenticationPage(),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
-            decoration: BoxDecoration(
-              color: AppColors.primary,
-            ),
-          ),
-          ListTile(
-            leading: Icon(Icons.play_arrow),
-            //title: Text(AppLocalizations.of(context).translate('home')),
-            title: Text("Capacitação Geral"),
-            selected: 0 == _selectedIndex,
-            onTap: () {
-              _onSelectItem(0);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.build),
-            title: Text("Bla Bla Bla"),
-            selected: 1 == _selectedIndex,
-            onTap: () {
-              _onSelectItem(1);
-            },
-          ),
-          ListTile(
-            leading: Icon(Icons.autorenew),
-            title: Text("Bla Bla Bla"),
-            selected: 2 == _selectedIndex,
-            onTap: () {
-              _onSelectItem(2);
-            },
           ),
         ],
       ),
